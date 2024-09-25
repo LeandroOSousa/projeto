1ce635f08e647e13d06571c55fb4d2c8a9b27c5d
Leandro <lean.sousa@gmail.com>
2024-09-25 14:19:05 -0300

    several updates

--- projeto/src/main/java/br/com/sousa/projeto/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.sousa.projeto.repository;
-
-import br.com.sousa.projeto.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface UserRepository extends JpaRepository <UserEntity, Long>{
-}
